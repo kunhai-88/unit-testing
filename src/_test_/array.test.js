219e04acccd30d9eb6518e5c8f48e3fdf4f0eb24
杨坤海 <384037191@qq.com>
2019-07-24 16:38:16 +0800

    test array

---
+++ src/_test_/array.test.js
@@ -0,0 +1,23 @@
+const a = [1, 2, 3, 4];
+const b = [1, 2, 3, 4];
+const c = {
+  a,
+  o: {
+    name: "hello"
+  }
+};
+const d = {
+  a,
+  o: {
+    name: "hello"
+  }
+};
+
+describe("Array", () => {
+  test("a equal b", () => {
+    expect(a).toEqual(b);
+  });
+  test("c deep equal d", () => {
+    expect(c).toEqual(d);
+  });
+});
